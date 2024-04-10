0643dcdbb6885a475ab29928906feb3cce47ba55
tolmana1 <166535231+tolmana1@users.noreply.github.com>
2024-04-10 13:16:18 -0600

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+
+
+
+// JavaScript to handle navbar sliding down on scroll 
+// this code doesn't work so
+
+//let prevScrollPos = window.scrollY || window.pageYOffset;
+
+//window.onscroll = function() {
+  //  let currentScrollPos = window.scrollY || window.pageYOffset;
+  //  if (prevScrollPos > currentScrollPos) {
+  //      document.getElementById("navbar").classList.add("active");
+  //  } else {
+  //      document.getElementById("navbar").classList.remove("active");
+  //  }
+  //  prevScrollPos = currentScrollPos;
+//}
+
